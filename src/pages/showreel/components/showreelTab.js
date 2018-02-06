f8423d22044f54c9c2e880c3b300aa5a3a1ccf5b
Joel <joel.balmer@gmail.com>
2018-02-06 23:37:38 +0000

    began adding files for making showreel a tabbed element

---
+++ src/pages/showreel/components/showreelTab.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ShowreelTab extends React.component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>hello</h1>
+			</div>
+		);
+	}
+}
