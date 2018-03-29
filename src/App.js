d0f3fe2b8bb6030d5227fdf3de2876e9acc0bd65
Joel <joel.balmer@gmail.com>
2018-03-29 22:13:19 +0100

    added prettier, which formatted code, etc'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import Navbar from './components/navbar/navbar.js';
-import Showreel from './pages/showreel/showreel.js';
-import About from './pages/about/about.js';
-import Contact from './pages/contact/contact.js';
-
+import Navbar from "./components/navbar/navbar.js";
+import Showreel from "./pages/showreel/showreel.js";
+import About from "./pages/about/about.js";
+import Contact from "./pages/contact/contact.js";
 
 class App extends Component {
   constructor(props) {
@@ -19,38 +18,33 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick (menuItem) {
-    this.setState({ currentPage: menuItem })
+  handleClick(menuItem) {
+    this.setState({ currentPage: menuItem });
 
     //swap out/content
   }
 
   render() {
-
     return (
       <div className="App">
         <header className="App-header">
           <div className="menu">
-            <Navbar onClick={this.handleClick} currentPage={ this.state.currentPage }/>
+            <Navbar
+              onClick={this.handleClick}
+              currentPage={this.state.currentPage}
+            />
           </div>
         </header>
 
         <div className="body">
           <h1 className="App-intro">Joel Balmer Music</h1>
           <h2>Freelance composer for TV and film</h2>
-          
-          {this.state.currentPage === "Showreel" &&
-            <Showreel />
-          }
 
-          {this.state.currentPage === "About" &&
-            <About />
-          }
+          {this.state.currentPage === "Showreel" && <Showreel />}
 
-          {this.state.currentPage === "Contact" &&
-            <Contact />
-          }
+          {this.state.currentPage === "About" && <About />}
 
+          {this.state.currentPage === "Contact" && <Contact />}
         </div>
       </div>
     );
